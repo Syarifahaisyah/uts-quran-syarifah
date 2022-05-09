9d43a6f1120c099665fc611f34cb7e167d7eab5a
syarifah aisyah <12050120380@student.uin-suska.ac.id>
2022-05-10 01:47:08 +0700

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,33 @@
+<template>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous" />
+  <div class="home bg-dark"></div>
+  <img src="../assets/logoo.jpeg" class="img-fluid" alt="..." />
+  <div class="container card mb-3 mt-3" style="width: 47rem">
+    <h2 class="mt-2"><b>KEUTAMAAN MEMBACA AL-QUR'AN </b></h2>
+    <p class="card bg-primary p-3 ms-2 me-2 mt-3">
+      Islam Muhyiddin Abu Zakariyya Yahya bin Syaraf An-Nawawi dalam kitabnya, Riyaadhus-Shaalihiin, membuat bab khusus tentang Keutamaan Membaca Al-Qur'an, di antaranya: Pertama, Al-Qur’an akan menjadi syafaat atau penolong di hari kiamat
+      untuk para pembacanya.<br /><br />
+
+      عن أَبي أُمامَةَ رضي اللَّه عنهُ قال : سمِعتُ رسولَ اللَّهِ صَلّى اللهُ عَلَيْهِ وسَلَّم يقولُ : « اقْرَؤُا القُرْآنَ فإِنَّهُ يَأْتي يَوْم القيامةِ شَفِيعاً لأصْحابِهِ » رواه مسلم <br /><br />
+      Abu Amamah ra, aku mendengar Rasulullah saw. bersabda, “Bacalah Al-Qur’an, karena sesungguhnya ia akan menjadi syafaat bagi para pembacanya di hari kiamat.” (HR. Muslim) Syekhul Islam Muhyiddin Abu Zakariyya Yahya bin Syaraf An-Nawawi
+      dalam kitabnya, Riyaadhus-Shaalihiin, membuat bab khusus tentang Keutamaan Membaca Al-Qur'an, di antaranya: Pertama, Al-Qur’an akan menjadi syafaat atau penolong di hari kiamat untuk para pembacanya.<br /><br />
+      قال : سمِعتُ رسولَ اللَّهِ صَلّى اللهُ عَلَيْهِ وسَلَّم يقولُ : « اقْرَؤُا القُرْآنَ فإِنَّهُ يَأْتي يَوْم القيامةِ شَفِيعاً لأصْحابِهِ » رواه مسلم <br /><br />Dari Abu Amamah ra, aku mendengar Rasulullah saw. bersabda, “Bacalah
+      Al-Qur’an, karena sesungguhnya ia akan menjadi syafaat bagi para pembacanya di hari kiamat.” (HR. Muslim); Kedua, orang yang mempelajari dan mengajarkan Al-Qur’an merupakan sebaik-baik manusia.<br /><br />
+      عن عثمانَ بن عفانَ رضيَ اللَّه عنهُ قال : قالَ رسولُ اللَّهِ صَلّى اللهُ عَلَيْهِ وسَلَّم : « خَيركُم مَنْ تَعَلَّمَ القُرْآنَ وَعلَّمهُ » رواه البخاري <br /><br />Dari Usman bin Affan ra, Rasulullah saw. bersabda, “Sebaik-baik kalian
+      adalah yang mempelajari al-Qur’an dan mengajarkannya.” (HR. Tirmidzi); Ketiga, untuk orang-orang yang mahir membaca Al-Qur’an, maka kelak ia akan bersama para malaikat-Nya;<br /><br />
+      عن عائشة رضي اللَّه عنها قالتْ : قال رسولُ اللَّهِ صَلّى اللهُ عَلَيْهِ وسَلَّم : « الَّذِي يَقرَأُ القُرْآنَ وَهُو ماهِرٌ بِهِ معَ السَّفَرةِ الكرَامِ البررَةِ » متفقٌ عليه .<br /><br />
+      Dari Aisyah ra, berkata; bahwa Rasulullah saw. bersabda, “Orang yang membaca Al-Qur’an dan ia mahir membacanya, maka kelak ia akan bersama para malaikat yang mulia lagi taat kepada Allah.” (HR. Bukhari Muslim); Keempat, untuk mereka
+      yang belum lancar dalam membaca dan mengkhatamkan Al-Qur’an, tidak boleh bersedih, sebab Allah tetap berikan dua pahala.<br /><br />
+      « وَاٌلَذِي يَقُراٌ القُرانَ وَيَتَتَعتَعُ فِيه وَهُوَ عَلَيهِ شَاقٌ لَه اَجَران » متفقٌ عليه <br /><br />Rasulullah bersabda, “Dan orang yang membaca Al-Qur’an, sedang ia masih terbata-bata lagi berat dalam membacanya, maka ia akan
+      mendapatkan dua pahala.” (HR. Bukhari Muslim); Kelima, Al-Qur’an dapat meningkatkan derajat kita di mata Allah.<br /><br />
+      عن عمرَ بن الخطابِ رضي اللَّه عنهُ أَنَّ النَّبِيَّ صَلّى اللهُ عَلَيْهِ وسَلَّم قال : « إِنَّ اللَّه يرفَعُ بِهذَا الكتاب أَقواماً ويضَعُ بِهِ آخَرين » رَوَاهُ مُسْلِمُ <br /><br />Dari Umar bin Khatab ra. Rasulullah saw. bersabda,:
+      “Sesungguhnya Allah SWT. akan mengangkat derajat suatu kaum dengan kitab ini (Al-Qur’an), dengan dengannya pula Allah akan merendahkan kaum yang lain.” (HR. Muslim)
+    </p>
+  </div>
+</template>
+<style>
+p {
+  text-align: justify;
+  color: white;
+}
+</style>
